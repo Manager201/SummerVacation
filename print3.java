47eaeed2af2f138f972ab27b732a504e921e724d
Manager201 <82263454+Manager201@users.noreply.github.com>
2023-05-27 12:40:00 +0530

    summer vacation

--- print3.java
+++ print3.java
@@ -1,1 +1,19 @@
-
+class print3
+{
+    void main()
+    {
+        int i,j,k;
+        for(i=1;i<=5;i++)
+        {
+            for(j=1;j<i;j++)
+            {
+                System.out.print(" ");
+            }
+            for(k=5;k>=i;k--)
+            {
+                System.out.print("# ");
+            }
+            System.out.println();
+        }
+    }
+}
