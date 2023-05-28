b23a05ece569ed1a0d85f78750f3c7fd03babe0c
Manager201 <82263454+Manager201@users.noreply.github.com>
2023-05-28 11:34:42 +0530

    summer vacation

--- specialdigit.java
+++ specialdigit.java
@@ -1,21 +1,23 @@
 import java.util.*;
-
 class specialdigit {
-    void main() {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter a number");
         int n = sc.nextInt();
         int s = 0, p = 1, d, m = n;
-            while (n != 0) 
-            {
-                d = n % 10;
-                s = s + d;
-                p = p * d;
-                n = n / 10;
-            }
-            if (s + p == m)
-                System.out.println("Special 2-digit number");
-            else
-                System.out.println("It is not a Special 2-digit number");
+        while (n != 0) 
+        {
+            d = n % 10;
+            s = s + d;
+            p = p * d;
+            n = n / 10;
+        }
+        if (s + p == m)
+
+            System.out.println("Yes! Sum of digit Number");
+
+        else
+            System.out.println("Not! Special 2-digit Number");
     }
 }
+
