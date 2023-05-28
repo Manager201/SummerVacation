14faf23b8f94a4867a2eebb96f7c267543e063b9
Manager201 <82263454+Manager201@users.noreply.github.com>
2023-05-28 11:30:21 +0530

    summer vacation

--- specialdigit.java
+++ specialdigit.java
@@ -6,8 +6,6 @@ class specialdigit {
         System.out.println("Enter a number");
         int n = sc.nextInt();
         int s = 0, p = 1, d, m = n;
-        if(n>=10&&n<=99) 
-        {
             while (n != 0) 
             {
                 d = n % 10;
@@ -19,7 +17,5 @@ class specialdigit {
                 System.out.println("Special 2-digit number");
             else
                 System.out.println("It is not a Special 2-digit number");
-        } else
-            System.out.println("It is not a 2-digit number");
     }
 }
